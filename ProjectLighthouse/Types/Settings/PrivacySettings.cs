0f28116178214b7c9c98d9914a69e54f45564a2f
jvyden <jvyden@jvyden.xyz>
2021-12-12 18:08:19 -0500

    Make PrivacySettings serialize properly

--- ProjectLighthouse/Types/Settings/PrivacySettings.cs
+++ ProjectLighthouse/Types/Settings/PrivacySettings.cs
@@ -2,6 +2,8 @@ using System.Xml.Serialization;
 
 namespace LBPUnion.ProjectLighthouse.Types.Settings
 {
+    [XmlRoot("privacySettings")]
+    [XmlType("privacySettings")]
     public class PrivacySettings
     {
         [XmlElement("levelVisibility")]
